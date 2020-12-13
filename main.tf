3519ce722ec31ad682d67928f0601721c09eb20e
yeagerghost <yeagerghost@gmail.com>
2020-12-13 18:37:40 -0500

    first commit

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-1"
+}
+
+
+resource "aws_elastic_beanstalk_application" "application" {
+  name        = "my-awesome-app"
+  description = "This is my awesome app"
+}
+
+
+resource "aws_elastic_beanstalk_environment" "environment" {
+  name                = "my-awesome-environment"
+  application         = aws_elastic_beanstalk_application.application.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.1.3 running Python 3.7"
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+
+  // Add environment variables if provided
+  dynamic "setting" {
+    for_each = var.env_vars
+    content {
+      namespace = "aws:elasticbeanstalk:application:environment"
+      name      = setting.key
+      value     = setting.value
+    }
+  }
+
+}
+
---
+++ variables.auto.tfvars
@@ -0,0 +1,6 @@
+env_vars = {
+  "DB_HOST"         = "xxxxxxxxxxxxxx"
+  "DB_USERNAME"     = "yyyyyyyyyyyyy"
+  "DB_PASSWORD"     = "zzzzzzzzzzzzzzzzzzz"
+  "ANOTHER_ENV_VAR" = "123456789"
+}
